c27f84663fcb40edb19d571f16918409724348d2
jedrzejiwanicki <iwanicki.jedrzej@gmail.com>
2018-09-02 22:40:19 +0200

    modify script

--- server/server.js
+++ server/server.js
@@ -6,14 +6,19 @@ const app = express();
 
 app.post('/gh', (req, res) => {
   console.log('Push intercepted, performing repo update');
+  res.status(200).send('OK')
+
   shell.exec('cd /home/video-conferencing');
   shell.exec('git pull origin master');
   shell.rm('-rf', 'node_modules');
-  shell.exec('npm install');
-  shell.exec('ng build');
-  shell.exec('pm2 restart video-conferencing');
 
-  res.status(200).send('OK')
+  console.log('node_modules removed');
+  shell.exec('npm install');
+  console.log('node_modules installed');
+  shell.exec('ng build');
+  console.log('angular build');
+  shell.exec('pm2 restart video-conferencing');
+  console.log('process restarted');
 
 });
 
